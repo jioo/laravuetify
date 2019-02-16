7aa0cdf486ccdfbf12d3855a0bfd3dfce59e6c3e
jioo <jio_qzn@yahoo.com>
2019-02-16 15:31:09 +0800

    Removed imports in routes.js - Removed bootstrap and jquery packages

--- resources/js/router/routes.js
+++ resources/js/router/routes.js
@@ -1,15 +1,3 @@
-const Welcome = () => import('~/pages/welcome').then(m => m.default || m)
-const Login = () => import('~/pages/auth/login').then(m => m.default || m)
-const Register = () => import('~/pages/auth/register').then(m => m.default || m)
-const PasswordEmail = () => import('~/pages/auth/password/email').then(m => m.default || m)
-const PasswordReset = () => import('~/pages/auth/password/reset').then(m => m.default || m)
-const NotFound = () => import('~/pages/errors/404').then(m => m.default || m)
-
-const Home = () => import('~/pages/home').then(m => m.default || m)
-const Settings = () => import('~/pages/settings/index').then(m => m.default || m)
-const SettingsProfile = () => import('~/pages/settings/profile').then(m => m.default || m)
-const SettingsPassword = () => import('~/pages/settings/password').then(m => m.default || m)
-
 export default [
   { path: '/login', name: 'login', component: () => import('~/pages/login').then(m => m.default || m) },
 
